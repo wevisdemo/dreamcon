c3dc593f2377e436e2ebb41ef4fe4a7e9fbc6b32
Petch Songpon <petch.s@finnomena.com>
2025-02-22 15:36:07 +0700

    feat: add connect line

--- src/pages/Topic.tsx
+++ src/pages/Topic.tsx
@@ -6,22 +6,25 @@ import { mockComments1, mockComments2, mockComments3 } from "../data/comment";
 export default function Topic() {
   const comments = useState([{ name: "John Doe", comment: "I agree with" }]);
   return (
-    <div className="bg-[#6EB7FE] min-h-screen min-w-screen flex flex-col items-center ">
+    <section className="py-[24px] bg-[#6EB7FE] min-h-screen min-w-screen flex flex-col items-center">
       <div className="max-w-[920px]">
-        <div className="header-section relative overflow-hidden flex justify-end">
-          <div className="max-w-[892px] flex justify-end z-10">
+        <div className="header-section pl-[24px] relative overflow-hidden flex justify-end">
+          <div className=" flex justify-end z-10">
             <TopicCard />
           </div>
-          <div className="absolute w-[50%] left-0 top-[50%] rounded-[16px] border-solid border-[2px] border-blue3 h-full"></div>
+          <div className="absolute w-[50%] left-0 top-[50%] rounded-[16px] border-solid border-[2px] border-blue3 h-[100vh]"></div>
         </div>
-        <div className="comment-section mt-[10px]">
+        <div className="comment-section pl-[24px] py-[10px] overflow-hidden ">
           <p className="text-[16px] wv-bold wv-ibmplex mt-[24px]">
             {comments.length} ความคิดเห็น
           </p>
           <div className="comment-section-body flex flex-col gap-[24px]">
             <div className="view-wrapper mt-[16px]">
-              <div className="bg-lightGreen px-[10px] py-[4px] w-fit rounded-[16px] text[13px]">
-                {mockComments1.length} เห็นด้วย
+              <div className="relative">
+                <p className="relative bg-lightGreen px-[10px] py-[4px] w-fit rounded-[16px] text[13px] z-10">
+                  {mockComments1.length} เห็นด้วย
+                </p>
+                <div className="absolute w-[40px] left-[-24px] bottom-[50%] rounded-bl-[16px] border-solid border-l-[2px] border-b-[2px] border-blue3 h-[1000vh]"></div>
               </div>
               <CommentWrapper
                 comments={mockComments1}
@@ -30,8 +33,11 @@ export default function Topic() {
               />
             </div>
             <div className="view-wrapper">
-              <div className="bg-lightYellow px-[10px] py-[4px] w-fit rounded-[16px] text[13px]">
-                {mockComments2.length} เห็นด้วยบางส่วน
+              <div className="relative">
+                <p className="relative bg-lightYellow px-[10px] py-[4px] w-fit rounded-[16px] text[13px] z-10">
+                  {mockComments2.length} เห็นด้วยบางส่วน
+                </p>
+                <div className="absolute w-[40px] left-[-24px] bottom-[50%] rounded-bl-[16px] border-solid border-l-[2px] border-b-[2px] border-blue3 h-[1000vh]"></div>
               </div>
               <CommentWrapper
                 comments={mockComments2}
@@ -39,10 +45,12 @@ export default function Topic() {
                 isLastChildOfParent
               />
             </div>
-            {/* section */}
             <div className="view-wrapper">
-              <div className="bg-lightRed px-[10px] py-[4px] w-fit rounded-[16px] text[13px]">
-                {mockComments3.length} ไม่เห็นด้วย
+              <div className="relative">
+                <p className="relative bg-lightRed px-[10px] py-[4px] w-fit rounded-[16px] text[13px] z-10">
+                  {mockComments3.length} ไม่เห็นด้วย
+                </p>
+                <div className="absolute w-[40px] left-[-24px] bottom-[50%] rounded-bl-[16px] border-solid border-l-[2px] border-b-[2px] border-blue3 h-[1000vh]"></div>
               </div>
               <CommentWrapper
                 comments={mockComments3}
@@ -53,6 +61,6 @@ export default function Topic() {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
