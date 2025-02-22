4afa315e915ae52aff9e03704dc392d0605eb7a2
Petch Songpon <petch.s@finnomena.com>
2025-02-22 16:32:08 +0700

    feat: separate section in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,34 @@
+import TopicTemplate from "../components/topic/TopicTemplate";
 import TopicWrapper from "../components/TopicWrapper";
+import { mockTopic1 } from "../data/topic";
 
 export default function Home() {
   return (
-    <div className="bg-[#6EB7FE] min-h-screen min-w-screen flex flex-col items-center ">
-      <TopicWrapper />
+    <div className="min-w-scree flex">
+      <section className="bg-blue2 w-[60%] min-h-screen flex flex-col items-center ">
+        <TopicWrapper />
+      </section>
+      <section className="w-[40%] min-h-screen flex flex-col items-center">
+        <div className="w-full px-[10px] py-[4px] bg-gray2 flex gap-[10px]">
+          <button>
+            <img
+              className="w-[24px] h-[24px]"
+              src="/icon/double-arrow-right.svg"
+              alt="double-arrow-right-icon"
+            />
+          </button>
+          <button>
+            <img
+              className="w-[24px] h-[24px]"
+              src="/icon/expand-wide.svg"
+              alt="expand-icon"
+            />
+          </button>
+        </div>
+        <div className="p-[24px] bg-blue4 ">
+          <TopicTemplate topic={mockTopic1} />
+        </div>
+      </section>
     </div>
   );
 }
--- src/pages/Topic.tsx
+++ src/pages/Topic.tsx
@@ -4,8 +4,8 @@ import { mockTopic1 } from "../data/topic";
 
 export default function Topic() {
   return (
-    <template className="py-[24px] bg-[#6EB7FE] min-h-screen min-w-screen flex flex-col items-center">
+    <div className="py-[24px] bg-[#6EB7FE] min-h-screen min-w-screen flex flex-col items-center">
       <TopicTemplate topic={mockTopic1} />
-    </template>
+    </div>
   );
 }
