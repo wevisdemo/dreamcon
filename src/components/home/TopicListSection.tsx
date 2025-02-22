1c5c6c26754430a5a4315de61060d750c704161e
Petch Songpon <petch.s@finnomena.com>
2025-02-22 17:40:08 +0700

    feat: set layout of TopicListSection in Home page

---
+++ src/components/home/TopicListSection.tsx
@@ -0,0 +1,19 @@
+import TopicWrapper from "../TopicWrapper";
+
+export default function TopicListSection() {
+  return (
+    <div className="flex flex-col items-center gap-[24px]">
+      <button className="flex items-center gap-[8px] py-[10px] px-[60px] bg-blue6 rounded-[48px] text-white">
+        <img
+          className="w-[24px] h-[24px]"
+          src="/icon/plus.svg"
+          alt="plus-icon"
+        />
+        <span className="text-[16px] wv-bold wv-ibmplex">
+          เพิ่มข้อถกเถียงใหม่
+        </span>
+      </button>
+      <TopicWrapper />
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
+import TopicListSection from "../components/home/TopicListSection";
 import TopicTemplate from "../components/topic/TopicTemplate";
-import TopicWrapper from "../components/TopicWrapper";
 import { mockTopic1 } from "../data/topic";
 
 export default function Home() {
   return (
-    <div className="min-w-scree flex">
-      <section className="bg-blue2 w-[60%] min-h-screen flex flex-col items-center ">
-        <TopicWrapper />
+    <div className="min-w-screen flex">
+      <section className="bg-blue2 w-[60%] min-h-screen flex flex-col items-center p-[60px]">
+        <TopicListSection />
       </section>
       <section className="w-[40%] min-h-screen flex flex-col items-center">
         <div className="w-full px-[10px] py-[4px] bg-gray2 flex gap-[10px]">
