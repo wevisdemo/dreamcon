a73d92230259dbb3333e1273113f68d74d93cdc9
Petch Songpon <petch.s@finnomena.com>
2025-03-02 14:27:29 +0700

    feat: add hover and onclick addComment, menu to commetCard

--- src/components/topic/CommentCard.tsx
+++ src/components/topic/CommentCard.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Comment, CommentView } from "../../types/comment";
+import BubblePlusIcon from "../icon/BubblePlusIcon";
 
 interface PropTypes {
   comment: Comment;
@@ -7,8 +9,19 @@ interface PropTypes {
   roundedTr?: boolean;
   roundedBl?: boolean;
   roundedBr?: boolean;
+  onClickAddComment: () => void;
+  onClickMenu: () => void;
 }
 export default function CommentCard(props: PropTypes) {
+  const [hovered, setHovered] = useState(false);
+  const onClickAddComment = () => {
+    props.onClickAddComment();
+  };
+
+  const onClickMenu = () => {
+    props.onClickMenu();
+  };
+
   const viewColor = () => {
     switch (props.comment.comment_view) {
       case CommentView.AGREE:
@@ -31,13 +44,39 @@ export default function CommentCard(props: PropTypes) {
 
   return (
     <div
-      className={`p-[10px]  ${roundedClass()} text[13px] flex items-center`}
+      className={`p-[10px]  ${roundedClass()} text[13px] flex justify-between border-[2px] border-transparent hover:border-blue6 hover:cursor-pointer`}
       style={{ backgroundColor: props.bgColor }}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      onClick={() => console.log("click")}
     >
-      <div
-        className={`w-[12px] h-[12px] rounded-full bg-${viewColor()} mr-[10px]`}
-      />
-      <span>{props.comment.reason}</span>
+      <div className="flex flex-1 gap-[10px]">
+        <div className={`w-[12px] h-[12px] rounded-full bg-${viewColor()}`} />
+        <span className="flex-1">{props.comment.reason}</span>
+      </div>
+      <div className="flex items-start justify-between w-[40px]">
+        {hovered && (
+          <>
+            <BubblePlusIcon
+              color="#979797"
+              className="w-[18px] h-[18px]"
+              onClick={(e) => {
+                e.stopPropagation();
+                onClickAddComment();
+              }}
+            />
+            <img
+              src="/icon/menu.svg"
+              alt="menu-icon"
+              className="w-[18px] h-[18px]"
+              onClick={(e) => {
+                e.stopPropagation();
+                onClickMenu();
+              }}
+            />
+          </>
+        )}
+      </div>
     </div>
   );
 }
