12fe2c940fc39b95159cd41862185096240015c4
Petch Songpon <petch.s@finnomena.com>
2025-03-02 18:46:43 +0700

    feat: integrate popover menu to commentCard

--- src/components/topic/CommentCard.tsx
+++ src/components/topic/CommentCard.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Comment, CommentView } from "../../types/comment";
 import BubblePlusIcon from "../icon/BubblePlusIcon";
-
+import Popover from "@mui/material/Popover";
+import MenuPopover from "../share/MenuPopover";
 interface PropTypes {
   comment: Comment;
   bgColor: string;
@@ -14,12 +15,36 @@ interface PropTypes {
 }
 export default function CommentCard(props: PropTypes) {
   const [hovered, setHovered] = useState(false);
+  const [anchorMenu, setAnchorMenu] = useState<null | HTMLElement>(null);
   const onClickAddComment = () => {
     props.onClickAddComment();
   };
 
-  const onClickMenu = () => {
-    props.onClickMenu();
+  const openMenu = Boolean(anchorMenu);
+  const popoverID = openMenu ? "comment-menu" : undefined;
+
+  const showOption = () => {
+    if (hovered) return true;
+    if (openMenu) return true;
+    return false;
+  };
+
+  const handleClickMenu = (event: React.MouseEvent<HTMLImageElement>) => {
+    setAnchorMenu(anchorMenu ? null : event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorMenu(null);
+  };
+
+  const handleDelete = () => {
+    console.log("delete");
+    handleCloseMenu();
+  };
+
+  const handleEdit = () => {
+    console.log("edit");
+    handleCloseMenu();
   };
 
   const viewColor = () => {
@@ -41,42 +66,65 @@ export default function CommentCard(props: PropTypes) {
     if (props.roundedBr) classes += "rounded-br-[16px] ";
     return classes;
   };
-
   return (
-    <div
-      className={`p-[10px]  ${roundedClass()} text[13px] flex justify-between border-[2px] border-transparent hover:border-blue6 hover:cursor-pointer`}
-      style={{ backgroundColor: props.bgColor }}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      onClick={() => console.log("click")}
-    >
-      <div className="flex flex-1 gap-[10px]">
-        <div className={`w-[12px] h-[12px] rounded-full bg-${viewColor()}`} />
-        <span className="flex-1">{props.comment.reason}</span>
+    <div className="relative">
+      <div
+        aria-describedby={popoverID}
+        className={`p-[10px]  ${roundedClass()} text[13px] flex justify-between border-[2px] border-transparent hover:border-blue6 hover:cursor-pointer`}
+        style={{ backgroundColor: props.bgColor }}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        onClick={() => console.log("click")}
+      >
+        <div className="flex flex-1 gap-[10px]">
+          <div className={`w-[12px] h-[12px] rounded-full bg-${viewColor()}`} />
+          <span className="flex-1">{props.comment.reason}</span>
+        </div>
+        <div className="flex items-start justify-between w-[40px]">
+          {showOption() && (
+            <>
+              <BubblePlusIcon
+                color="#979797"
+                className="w-[18px] h-[18px]"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onClickAddComment();
+                }}
+              />
+              <img
+                src="/icon/menu.svg"
+                alt="menu-icon"
+                className="w-[18px] h-[18px]"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleClickMenu(e);
+                }}
+              />
+            </>
+          )}
+        </div>
       </div>
-      <div className="flex items-start justify-between w-[40px]">
-        {hovered && (
-          <>
-            <BubblePlusIcon
-              color="#979797"
-              className="w-[18px] h-[18px]"
-              onClick={(e) => {
-                e.stopPropagation();
-                onClickAddComment();
-              }}
-            />
-            <img
-              src="/icon/menu.svg"
-              alt="menu-icon"
-              className="w-[18px] h-[18px]"
-              onClick={(e) => {
-                e.stopPropagation();
-                onClickMenu();
-              }}
-            />
-          </>
-        )}
-      </div>
+      <Popover
+        id={popoverID}
+        open={openMenu}
+        anchorEl={anchorMenu}
+        onClose={handleCloseMenu}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        className="drop-shadow-none"
+        classes={{ paper: "box-1" }}
+      >
+        <MenuPopover
+          onClickDelete={() => handleDelete}
+          onClickEdit={() => handleEdit}
+        />
+      </Popover>
     </div>
   );
 }
