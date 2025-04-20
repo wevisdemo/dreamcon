b185cba8d96e0d12ff2c686a58d537a87d12e5c5
petchill <songpon111333@gmail.com>
2025-04-20 23:41:31 +0700

    fix: og

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BASE_URL } from "./const/app";
+import { HelmetProvider, Helmet } from "@dr.pogodin/react-helmet";
 
 const ogTitle = "DreamCon";
 const ogDescription = "พาความฝันของพวกเรา มาสร้างอนาคตประเทศไทยไปด้วยกัน";
@@ -11,20 +12,24 @@ const ogUrl = new URL("", BASE_URL).href;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <title>{ogTitle}</title>
-    <meta property="og:title" content={ogTitle} />
-    <meta property="og:description" content={ogDescription} />
-    <meta property="og:image" content={ogImage} />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content={ogUrl} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content={ogTitle} />
-    <meta name="twitter:description" content={ogDescription} />
-    <meta name="twitter:image" content={ogImage} />
-    <meta name="twitter:image:alt" content={ogTitle} />
-    <meta name="twitter:url" content={ogUrl} />
-    <main className="wv-ibmplexlooped">
-      <App />
-    </main>
+    <HelmetProvider>
+      <Helmet>
+        <title>{ogTitle}</title>
+        <meta property="og:title" content={ogTitle} />
+        <meta property="og:description" content={ogDescription} />
+        <meta property="og:image" content={ogImage} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={ogUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={ogTitle} />
+        <meta name="twitter:description" content={ogDescription} />
+        <meta name="twitter:image" content={ogImage} />
+        <meta name="twitter:image:alt" content={ogTitle} />
+        <meta name="twitter:url" content={ogUrl} />
+      </Helmet>
+      <main className="wv-ibmplexlooped">
+        <App />
+      </main>
+    </HelmetProvider>
   </StrictMode>
 );
