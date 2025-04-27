70660d46790319e1a314762bbc86dd7a5c319640
petchill <songpon111333@gmail.com>
2025-04-27 22:40:10 +0700

    fix: edit default state of empty topic

--- src/components/allTopic/EmptyTopic.tsx
+++ src/components/allTopic/EmptyTopic.tsx
@@ -2,7 +2,7 @@ export default function EmptyTopic() {
   return (
     <div className="flex flex-col items-center justify-center h-full min-h-[200px] w-full border-[2px] border-dashed border-white rounded-[16px]">
       <p className="text-white wv-ibmplex wv-bold text-[16px]">
-        ยังไม่มีข้อถกเถียงของวงสนทนานี้
+        ยังไม่มีข้อถกเถียง
       </p>
     </div>
   );
